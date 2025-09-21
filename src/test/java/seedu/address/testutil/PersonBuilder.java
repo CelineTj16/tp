efa1ba0ef5001b9ce5b7693b3f3fc60089adb63e
celine <josephinecelinetj@gmail.com>
2025-09-21 16:16:48 +0800

    Fix CheckStyle

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -16,7 +16,6 @@ import seedu.address.model.util.SampleDataUtil;
  * A utility class to help with building Person objects.
  */
 public class PersonBuilder {
-
     public static final String DEFAULT_NAME = "Amy Bee";
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
@@ -27,12 +26,10 @@ public class PersonBuilder {
     private Phone phone;
     private Email email;
     private Address address;
-    public Remark remark;
+    private Remark remark;
     private Set<Tag> tags;
 
-    /**
-     * Creates a {@code PersonBuilder} with the default details.
-     */
+    /** Creates a {@code PersonBuilder} with the default details. */
     public PersonBuilder() {
         name = new Name(DEFAULT_NAME);
         phone = new Phone(DEFAULT_PHONE);
@@ -42,9 +39,7 @@ public class PersonBuilder {
         tags = new HashSet<>();
     }
 
-    /**
-     * Initializes the PersonBuilder with the data of {@code personToCopy}.
-     */
+    /** Initializes the PersonBuilder with the data of {@code personToCopy}. */
     public PersonBuilder(Person personToCopy) {
         name = personToCopy.getName();
         phone = personToCopy.getPhone();
@@ -54,49 +49,37 @@ public class PersonBuilder {
         tags = new HashSet<>(personToCopy.getTags());
     }
 
-    /**
-     * Sets the {@code Name} of the {@code Person} that we are building.
-     */
+    /** Sets the {@code Name} of the {@code Person} that we are building. */
     public PersonBuilder withName(String name) {
         this.name = new Name(name);
         return this;
     }
 
-    /**
-     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
-     */
-    public PersonBuilder withTags(String ... tags) {
+    /** Parses the {@code tags} and sets them on the {@code Person} that we are building. */
+    public PersonBuilder withTags(String... tags) {
         this.tags = SampleDataUtil.getTagSet(tags);
         return this;
     }
 
-    /**
-     * Sets the {@code Address} of the {@code Person} that we are building.
-     */
+    /** Sets the {@code Address} of the {@code Person} that we are building. */
     public PersonBuilder withAddress(String address) {
         this.address = new Address(address);
         return this;
     }
 
-    /**
-     * Sets the {@code Phone} of the {@code Person} that we are building.
-     */
+    /** Sets the {@code Phone} of the {@code Person} that we are building. */
     public PersonBuilder withPhone(String phone) {
         this.phone = new Phone(phone);
         return this;
     }
 
-    /**
-     * Sets the {@code Email} of the {@code Person} that we are building.
-     */
+    /** Sets the {@code Email} of the {@code Person} that we are building. */
     public PersonBuilder withEmail(String email) {
         this.email = new Email(email);
         return this;
     }
 
-    /**
-     * Sets the {@code Remark} of the {@code Person} that we are building.
-     */
+    /** Sets the {@code Remark} of the {@code Person} that we are building. */
     public PersonBuilder withRemark(String remark) {
         this.remark = new Remark(remark);
         return this;
@@ -105,5 +88,4 @@ public class PersonBuilder {
     public Person build() {
         return new Person(name, phone, email, address, remark, tags);
     }
-
 }
