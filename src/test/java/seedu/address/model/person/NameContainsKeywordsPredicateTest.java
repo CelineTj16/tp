7a0716830de23ed2ab95982c74317cfe30111b41
celine <josephinecelinetj@gmail.com>
2025-10-22 23:55:56 +0800

    Add Tests for new Find Command

--- src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
@@ -82,4 +82,24 @@ public class NameContainsKeywordsPredicateTest {
         String expected = NameContainsKeywordsPredicate.class.getCanonicalName() + "{keywords=" + keywords + "}";
         assertEquals(expected, predicate.toString());
     }
+
+    @Test
+    public void test_substringMatch_returnsTrue() {
+        NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate(Collections.singletonList("li"));
+        assertTrue(predicate.test(new PersonBuilder().withName("Alice Bob").build()));
+    }
+
+    @Test
+    public void test_partnerNameMatch_client_returnsTrue() {
+        NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate(Collections.singletonList("Alex"));
+        Person client = new PersonBuilder().withName("Amira Tan").withType(PersonType.parse("CLIENT")).withPartner("Alex Ong").build();
+        assertTrue(predicate.test(client));
+    }
+
+    @Test
+    public void test_partnerNameMatch_vendor_returnsFalse() {
+        NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate(Collections.singletonList("Alex"));
+        Person vendor = new PersonBuilder().withName("Kai Catering").withType(PersonType.parse("VENDOR")).build();
+        assertFalse(predicate.test(vendor));
+    }
 }
